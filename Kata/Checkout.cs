a12ec75bc4610d7df1aacf13027ee216860d16d2
Yu Gan <454216847@qq.com>
2023-05-15 23:32:21 +1200

    modified

--- Kata/Checkout.cs
+++ Kata/Checkout.cs
@@ -10,10 +10,13 @@ using System.Threading.Tasks;
 
 namespace Kata
 {
+    /// <summary>
+    /// Scan and checkout items
+    /// </summary>
     public class Checkout : ICheckout
     {
-        public  Dictionary<string, int> itemsScanned;
-        public  Dictionary<string, IItem> itemsStorage;
+        public Dictionary<string, int> itemsScanned;
+        public Dictionary<string, IItem> itemsStorage;
 
         public Checkout(Dictionary<string, int> itemsScanned, Dictionary<string, IItem> itemsStorage)
         {
@@ -21,19 +24,27 @@ namespace Kata
             this.itemsStorage = itemsStorage;
         }
 
+        /// <summary>
+        /// Scan items
+        /// </summary>
+        /// <param name="sku"></param>
+        /// <exception cref="InvalidDataException"></exception>
         public void Scan(string sku)
         {
+            // Scan invalid sku
             if (!itemsStorage.ContainsKey(sku))
             {
                 Console.WriteLine("Invalid SKU");
-                throw new InvalidDataException("Invalid SKU");
+                throw new InvalidDataException("invalid sku");
             }
             else
             {
+                // Scan valid sku again
                 if (itemsScanned.ContainsKey(sku))
                 {
                     itemsScanned[sku]++;
                 }
+                // Scan valid sku first time
                 else
                 {
                     itemsScanned[sku] = 1;
@@ -42,37 +53,43 @@ namespace Kata
         }
 
 
-
+        /// <summary>
+        /// Calculate total price
+        /// </summary>
+        /// <returns></returns>
         public int CalculateTotalPrice()
         {
             int totalPrice = 0;
-
+            //get item price and count
             foreach (var item in itemsStorage.Values)
             {
                 int unitPrice = item.Price;
                 int itemCount = itemsScanned.GetValueOrDefault(item.SKU);
-                if (item.SpecialPrice?.Any()==true)
+                //if there is discount in item
+                if (item.SpecialPrice?.Any() == true)
                 {
-
+                    //get discount information
                     foreach (var discount in item.SpecialPrice)
                     {
                         int discountCount = discount.Key;
                         int discountPrice = discount.Value;
-
+                        //get discount
                         if (itemCount >= discountCount)
                         {
                             int regularCount = itemCount % discountCount;
                             int discountGroupCount = itemCount / discountCount;
                             totalPrice += discountGroupCount * discountPrice;
-                            itemCount= regularCount;
+                            itemCount = regularCount;
                             break;
                         }
+                        //not enough count for discount
                         else
                         {
                             totalPrice += itemCount * unitPrice;
                         }
                     }
                 }
+                //no discount
                 else
                 {
                     totalPrice += itemCount * unitPrice;
--- Kata/Program.cs
+++ Kata/Program.cs
@@ -10,6 +10,7 @@ namespace Kata
             Dictionary<string, int> itemsScanned = new Dictionary<string, int>();
             Dictionary<string, IItem> itemsStorage = new Dictionary<string, IItem>();
             Checkout checkout = new Checkout(itemsScanned, itemsStorage);
+            //add items in storage
             checkout.itemsStorage = new Dictionary<string, IItem>
             {
                 {"A",new ItemA()},
@@ -17,15 +18,26 @@ namespace Kata
                 {"C",new ItemC()},
                 {"D",new ItemD()}
             };
+
             while (true)
             {
                 Console.WriteLine("Please enter the SKU (or leave empty to exit):");
                 string sku = Console.ReadLine();
+
+                //enter empty string to exit
                 if (string.IsNullOrEmpty(sku))
                 {
                     break;
                 }
-                checkout.Scan(sku);
+                //exit if scan invalid sku
+                try
+                {
+                    checkout.Scan(sku);
+                }
+                catch (InvalidDataException)
+                {
+                    break;
+                }
                 int totalPrice = checkout.CalculateTotalPrice();
                 Console.WriteLine($"Total Price:{totalPrice}");
             }
